ab833a9a95b9956fd170c60d0883fe14314d566c
Robin Nunkesser <robin.nunkesser@hshl.de>
2023-03-08 09:12:13 +0100

    Refactoring

---
+++ Italbytz.Ports.Exam/Italbytz.Ports.Exam/IStepwiseSolution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Italbytz.Ports.Exam
+{
+    public interface IStepwiseSolution<TSolution>
+    {
+        TSolution Solution { get; set; }
+        List<TSolution> Steps { get; set; }
+    }
+}
+
